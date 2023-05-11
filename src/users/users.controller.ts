cb6b3607f5f274a9d2f6a724544409758380599b
Elizaveta Kritskaya <post.kri@gmail.com>
2023-05-11 21:21:58 +0300

    add @UseGuards

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,16 @@
-import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
+import {Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, Put, UseGuards} from '@nestjs/common';
 import {UsersService} from "../services/users/users.service";
 import {User} from "../shema/user";
 import {UserDto} from "../dto/user-dto";
 import RejectedValue = jest.RejectedValue;
+import {AuthGuard} from "@nestjs/passport";
 
 @Controller('users')
 export class UsersController {
+
     constructor(private userService: UsersService) {
     }
+
     //конфликт с текущим роутером, если используем декоратор @Query
     @Get()
     getAllUsers(): Promise<User[]> {
@@ -21,31 +24,43 @@ export class UsersController {
 
     @Post()
     sendUser(@Body() data: UserDto): Promise<User> {
-
         return this.userService.checkRegUser(data.login).then((queryRes) => {
             console.log('data reg', queryRes)
             if (queryRes.length === 0) {
                 return this.userService.sendUser(data);
             } else {
                 console.log('err - user is exists')
-                return Promise.reject();
+                //Код ошибки
+                throw new HttpException({
+                    status: HttpStatus.CONFLICT,
+                    errorText: 'Пользователь уже зарегистрирован'
+                }, HttpStatus.CONFLICT)
             }
         });
 
     }
 
+    //применяется к методу
+    @UseGuards(AuthGuard('local'))
     @Post(":login")
-    authUser(@Body() data: UserDto, @Param('login') login): Promise<User | boolean>  {
-        return this.userService.checkAuthUser(data.login, data.psw).then((queryRes) => {
-            if (queryRes.length !== 0) {
-                return Promise.resolve(true);
-            } else {
-                console.log('err - user is exists')
-                return Promise.reject();
-            }
-        });
-
+    authUser(@Body() data: UserDto, @Param(':login') login): any{
+        return this.userService.login(data);
     }
+    // authUser(@Body() data: UserDto, @Param('login') login): Promise<User | boolean>  {
+    //     return this.userService.checkAuthUser(data.login, data.psw).then((queryRes) => {
+    //         if (queryRes.length !== 0) {
+    //             return Promise.resolve(true);
+    //         } else {
+    //             console.log('err - user is exists')
+    //             //Код ошибки
+    //             throw new HttpException({
+    //                 status: HttpStatus.CONFLICT,
+    //                 errorText: 'Пользователь не найден в базе'
+    //             }, HttpStatus.CONFLICT)
+    //             // return Promise.reject();}
+    //     });
+    //
+    // }
 
     @Put(":id")
     updateUsers(@Param("id") id, @Body() data): Promise<User> {
