e2da25c60b1ca156bacc4ec78eb82eb520e31435
Elizaveta Kritskaya <post.kri@gmail.com>
2023-05-11 21:18:46 +0300

    add PassportModule + JwtModule + AuthService

--- src/controllers/users/users.module.ts
+++ src/controllers/users/users.module.ts
@@ -3,10 +3,19 @@ import {UsersController} from "../../users/users.controller";
 import {UsersService} from "../../services/users/users.service";
 import {User, UserSchema} from "../../shema/user";
 import {MongooseModule} from "@nestjs/mongoose";
+import {AuthService} from "../../services/auth/auth.service";
+import {PassportModule} from "@nestjs/passport";
+import {JwtModule} from "@nestjs/jwt";
+import {jwtConstants} from "../../static/private/constants";
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+    imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }],),
+        PassportModule,
+    JwtModule.register({
+        secret: jwtConstants.secret,
+    })],
+
     controllers: [UsersController],
-    providers: [UsersService]
+    providers: [UsersService, AuthService]
 })
 export class UsersModule {}
